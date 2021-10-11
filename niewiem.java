16634ae4bf143c1ef81e4102d87a0f0d1f2639d7
Arek212 <76994019+Arek212@users.noreply.github.com>
2021-10-11 15:37:28 +0200

    koniec

--- niewiem.java
+++ niewiem.java
@@ -1,42 +1,71 @@
-public class niewiem {
-//    zadanie 1-----------------------
-//    public static void main(String[] args) {
-//        System.out.print(31+29+31);
-//    }
-//    zadanie 2-----------------------
-//    public static void main(String[] args) {
-//        int wynik =1;
-//        for(int i = 2; i<11;i++)wynik+=i;
-//        System.out.print(wynik);
-//    }
-//    zadanie 3------------------------
-//    public static void main(String[] args) {
-//        int wynik =1;
-//        for(int i = 2; i<11;i++)wynik*=i;
-//        System.out.print(wynik);
-//    }
-//    zadanie 4--------------------------
-//    public static void main(String[] args) {
-//        int wynik = 1000;
-//        for (int i = 0; i < 3; i++) {
-//            wynik *= 1.06;
-//            System.out.print(wynik+"\n");
+public class zadanie {
+    public static void main(String[] args) {
+        //zadanie 1
+//        System.out.println(31+29+31);
+        //zadanie 2
+//        int x=0;
+//        for (int i=0;i<=10;i++)
+//        {
+//            x+=i;
 //        }
-//    }
-//    zadanie 5-------------------------
-//    public static void main(String[] args) {
-//        System.out.print("+----+\n");
-//        System.out.print("|Java|\n");
-//        System.out.print("+----+");
-//    }
-//    zadanie 6------------------------
-//    public static void main(String[] args){
-//        System.out.print("  /////\n" +
-//                " +\"\"\"\"\"+\n" +
-//                "(| o o |)\n" +
-//                "  | ^ |\n" +
-//                " | `-' |\n" +
-//                " +-----+");
-//    }
-
+//        System.out.println(x);
+        //zadanie 3
+//        int x=1;
+//        for (int i=1;i<=10;i++)
+//        {
+//            x*=i;
+//        }
+//        System.out.println(x);
+        //zadanie 4
+//        float konto=3000;
+//        for(int i=1;i<=3;i++)
+//        {
+//            konto+=konto*0.06;
+//            System.out.println(i + " rok = "+konto);
+//        }
+        //zadanie 5
+//        System.out.println("______\n|JAVA|\n______");
+        //zadanie 6
+//        System.out.println("   /////    \n  +\"\"\"\"\"+  \n (| o o |)\n   | ^ |   \n  | `-' |  \n  +-----+  \n");
+        //zadanie 7
+//        System.out.println("      *                                 *\n" +
+//                "     * *           * *****    ******    * *\n" +
+//                "    *   *          **        *      *   **\n" +
+//                "   *******         *        **********  **\n" +
+//                "  *       *        *        *           * *\n" +
+//                " *         *       *         *     *    *  *\n" +
+//                "*           *      *          *****     *   *");
+        //zadanie 8
+//        System.out.println("   +    \n  + +   \n +   +  \n+-----+\n| .-. |\n| | | |\n+-+-+-+\n");
+        //zadanie 9
+//        System.out.println(" (-|-)        -----\n |   |       /Hello\\\n |   |       <Junior|\n  | |        \\Coder!/\n              -----");
+        //zadanie 10
+//        System.out.println("1.Cowboy Bebop\n2.Mushishi\n3.Monster");
+        //zadanie 11
+//        System.out.println("O chłopcu\n" +
+//                "Pan sobie kazał przywieść białogłowę,\n" +
+//                "Aby z nią mógł mieć tajemną rozmowe.\n" +
+//                "Czekawszy chłopca dobrze długą chwilę,\n" +
+//                "Tak zeby drugi uszedł był i milę,\n" +
+//                "Pojźrzy pod okno, a ci sobie radzi!\n" +
+//                "I r\"\"zecze z góry do onej czeladzi:\n" +
+//                "\"Po diable, synku, folgujesz tej paniej:\n" +
+//                "Jam kazał przywieść, a ty jedziesz na niej\".");
+        //zadanie 12
+//        System.out.println("* * * * * * ===========================================");
+//        System.out.println(" * * * * *  ===========================================");
+//        System.out.println("* * * * * * ===========================================");
+//        System.out.println(" * * * * *  ===========================================");
+//        System.out.println("* * * * * * ===========================================");
+//        System.out.println(" * * * * *  ===========================================");
+//        System.out.println("* * * * * * ===========================================");
+//        System.out.println(" * * * * *  ===========================================");
+//        System.out.println("* * * * * * ===========================================");
+//        System.out.println("=======================================================");
+//        System.out.println("=======================================================");
+//        System.out.println("=======================================================");
+//        System.out.println("=======================================================");
+//        System.out.println("=======================================================");
+//        System.out.println("=======================================================");
+    }
 }
